f1612cee56eac57ea37dcf554efc269e693adc06
guixiang <276935962@qq.com>
2023-10-13 18:35:40 +0800

    vite

---
+++ main.js
@@ -0,0 +1,14 @@
+import { counter } from './counter.js';
+/**
+ * 依赖预构建的为了解决vite识别不了commonjs规范格式，只识别esmodule，需要与构建到nodemodule的.vite中
+ * 依赖预构建
+ * vite找到依赖，然后调用esbuild（对js语法进行处理的一个库），将其他规范的代码转化为esmodule规范，然后放到当前目录下的nodemodule的.vite/deps
+ * 解决了三个问题
+ * 1. 不同的第三方包会使用不同的导出格式（vite没法约束）
+ * 2. 对路劲的处理上可以直接使用.vite/deps，方便路径重写
+ * 3. 网络多包传输的性能问题，因为他esbuild了（依赖又依赖其他依赖，导致会加载其他依赖，是原生js不敢支持node_modules的原因）
+ */
+import _ from 'lodash';
+import lodash from 'lodash-es';
+console.log('lodash', import.meta.env.VITE_APP_KEY);
+console.log('counter', counter);
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import viteBaseConfig from './build/vite.base.config';
+import viteDevConfig from './build/vite.dev.config';
+import viteProdConfig from './build/vite.prod.config';
+
+// 策略模式
+const envResolve = {
+  build: () => {
+    console.log('生产环境');
+    return { ...viteBaseConfig, ...viteProdConfig };
+  },
+  serve: () => {
+    console.log('开发环境');
+    return { ...viteDevConfig, ...viteDevConfig };
+  },
+};
+export default defineConfig(({ command }) => {
+  // console.log('process', process.env.VITE_APP_KEY);
+  return envResolve[command]();
+});
+
+// 语法提示
+// 类型注解
+/**
+ * @type import('vite').UserConfig
+ */
+// const viteConfig = {
+//   // 预构建配置
+//   optimizeDeps: {
+//     // 依赖不进行依赖预构建
+//     // exclude: ['lodash-es'],
+//   },
+// };
+// 关于环境的处理
